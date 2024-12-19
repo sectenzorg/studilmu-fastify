935c2673262ce32c54d1e7006d2c4158d059fb91
syarifabdillah <91821026+sectenzorg@users.noreply.github.com>
2024-12-19 12:13:46 +0700

    Update run-arm.sh

--- run-arm.sh
+++ run-arm.sh
@@ -4,8 +4,13 @@
 CONTAINER_NAME="lms-be-development"
 IMAGE_NAME="277707103873.dkr.ecr.ap-southeast-3.amazonaws.com/lms-be-development:latest"
 
+# Reset QEMU for multi-platform support
+echo "Initializing QEMU for multi-architecture support..."
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 # Check if the container already exists
 if docker ps -a --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
+  echo "Stopping and removing existing container..."
   docker stop $CONTAINER_NAME  # Stop the running container
   docker rm $CONTAINER_NAME    # Remove the container
 else
@@ -19,3 +24,10 @@ docker buildx build --platform linux/arm64 -t $IMAGE_NAME -f .devops/Dockerfile 
 # Start the container again (new or recreated)
 echo "Starting the container..."
 docker run -dit --platform linux/arm64 --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
+
+# Verify if the container started correctly
+docker ps -a | grep $CONTAINER_NAME
+if [ $? -ne 0 ]; then
+  echo "The container is not running. Checking logs..."
+  docker logs $CONTAINER_NAME
+fi
